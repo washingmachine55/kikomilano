dfd13e9cc066d655eff5c6b22ee64eb2c5685cb6
Ahmed Babar <hmed42@hotmail.com>
2026-01-28 01:31:37 +0500

    - Forgot password implemented

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { verifyInputFields } from '../middlewares/verifyInputFields.auth.js';
-import { loginUser, refreshToken, registerUser, verifyUserToken } from '../controllers/auth.controller.js';
+import { forgotPassword, loginUser, refreshToken, registerUser, resetPassword, verifyOTP, verifyUserToken } from '../controllers/auth.controller.js';
 import verifyToken from '../middlewares/verifyToken.auth.js';
 const router = express.Router();
 
@@ -12,12 +12,12 @@ const router = express.Router();
 router.post('/register', verifyInputFields, registerUser);
 router.post('/login', verifyInputFields, loginUser);
 router.post('/refresh', refreshToken);
-// router.post('/google-sign-in', verifyGoogleAuth)
 router.get('/verify-token', verifyToken, verifyUserToken);
+router.post('/forgot-password', verifyInputFields, forgotPassword)
+router.post('/verify-otp', verifyOTP)
+router.post('/reset-password', resetPassword)
 // router.get('/logout', logoutUser)
 // router.get('/resend-otp', verifyToken, resendOTP)
-// router.post('/verify-otp', verifyToken, verifyOTP)
 // router.get('/verify-access', verifyToken, verifyUserAccess)
-// router.post('/forgot-password', forgotPassword)
 
 export default router;
---
+++ src/services/auth/createForgotPasswordEmail.auth.service.js
@@ -0,0 +1,59 @@
+import pool from '../../config/db.js';
+
+import { TZDate } from "@date-fns/tz";
+import { formatDate, formatDistance } from "date-fns";
+import transporter from "../../config/mailTransporter.js";
+import { getRandomOTP } from "../../utils/getRandomOTP.js";
+
+export async function createForgotPasswordEmail(userId, userEmail) {
+	const conn = await pool.connect();
+
+	const currentTimestamp = new Date();
+	let expirationTimestamp = new Date();
+	const expiration_time = Number(process.env.OTP_EXPIRATION_TIME)
+	expirationTimestamp = new Date(expirationTimestamp.setMinutes(expirationTimestamp.getMinutes() + expiration_time))
+
+	const currentTimestampISO = currentTimestamp.toISOString().replace('T', ' ').replace('Z', '')
+	const expirationTimestampISO = expirationTimestamp.toISOString().replace('T', ' ').replace('Z', '')
+	const timeDifferenceForHumans = formatDistance(expirationTimestampISO, currentTimestampISO,
+		{
+			addSuffix: true,
+			includeSeconds: true,
+		}
+	)
+
+	const ConvertExpirationTimestampToLocal = TZDate.tz("Asia/Karachi", expirationTimestamp).toISOString()
+	const formattedExpirationTimestamp = formatDate(ConvertExpirationTimestampToLocal, 'PPPPpp').concat(" PKT")
+
+	const otp = getRandomOTP();
+
+	try {
+		try {
+			await conn.query('INSERT INTO tbl_users_otp(users_id,otp_value,date_sent,date_expiration) VALUES ($1,$2,$3,$4);', [userId, otp, currentTimestampISO, expirationTimestampISO]);
+		} catch (error) {
+			console.debug(error)
+		}
+
+		await transporter.sendMail({
+			from: '"Admin Sender" <test@example.com>',
+			to: userEmail,
+			subject: `Verify your Email: User ${userId}`,
+			text: "This is a test email sent via Nodemailer",
+			html: `
+			<p>This is a <b>test verification email</b> sent via Nodemailer!</p>
+			<br/>
+				<p>
+					Please enter the following OTP in the App to reset your password: 
+					<b>${otp}</b>
+				</p>
+			<br/>
+			<p>The OTP expires <b>${timeDifferenceForHumans}</b> on ${formattedExpirationTimestamp}</p>`,
+		});
+		
+	} catch (error) {
+		console.debug(error)
+	} finally {
+		conn.release();
+	}
+
+}
